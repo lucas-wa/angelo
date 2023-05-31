0f06634892f67aeee77c2fe27cd6f1f129e05ec9
lucas-wa <lucaswanderley600@gmail.com>
2023-05-31 00:19:35 -0300

    Styling page and adding prompt option

--- static/script.js
+++ static/script.js
@@ -1,27 +1,31 @@
-function test() {
-    fetch('/generateImage', {
-      headers : {
-          'Content-Type' : 'application/json'
-      },
-      method : 'POST', 
-      body : JSON.stringify( {  
-          'prompt' : 'a photograph of an astronaut riding a horse'
-      })
-  })
-  .then(function (response){ 
-  
-      if(response.ok) {  
-        // window.href = "/generateImage"
-          response.json() 
-          .then(function(response) {
-              console.log(response)
-          });
-      }
-      else {
-          throw Error('Something went wrong');
-      }
-  })
-  .catch(function(error) {
-      console.log(error);
-  });
-  }
+document.querySelector("form")
+    .addEventListener("submit", async e => {
+        e.preventDefault()
+
+        document.querySelector("button").innerHTML = "<div class='lds-ring'><div></div><div></div><div></div><div></div></div>"
+
+        try {
+
+            const prompt = document.querySlector("input").value
+
+            const response = await fetch('/', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    'prompt': prompt
+                })
+            }).json()
+
+
+            const { data } = response;
+
+            document.querySelector("img").src = data.image_raw;
+            document.querySelector("button").innerHTML = "Request"
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    })
