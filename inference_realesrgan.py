8f2cca9c466b057c1e7d695d053b219ec30984f4
lucas-wa <lucaswanderley600@gmail.com>
2023-07-05 18:01:58 -0300

    Inference file updated

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -9,122 +9,114 @@ from basicsr.utils.download_util import load_file_from_url
 from realesrgan import RealESRGANer
 from realesrgan.archs.srvgg_arch import SRVGGNetCompact
 
+from gfpgan import GFPGANer
+
+def detect_face(image):
+  face_cascade = cv2.CascadeClassifier()
+  face_cascade_name = "weights/haarcascade_frontalface_alt.xml"
+
+  if not os.path.isfile(face_cascade_name):
+    try:
+      file_url = 'https://raw.githubusercontent.com/avelino/python-opencv-detect/master/haarcascade_frontalface_alt.xml'
+      model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
+    except:
+      print('--(!)Error loading face cascade')
+      exit(0)
+
+  face_cascade.load(cv2.samples.findFile(face_cascade_name))
+
+  image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  image_gray = cv2.equalizeHist(image_gray)
+
+  faces = face_cascade.detectMultiScale(image_gray)
+
+  return True if len(faces) > 0 else False
+
 
 def main():
 
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, default='inputs', help='Input image or folder')
-    parser.add_argument(
-        '-n',
-        '--model_name',
-        type=str,
-        default='RealESRGAN_x4plus',
-        help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
-              'realesr-animevideov3 | realesr-general-x4v3'))
-    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
-    parser.add_argument(
-        '-dn',
-        '--denoise_strength',
-        type=float,
-        default=0.5,
-        help=('Denoise strength. 0 for weak denoise (keep noise), 1 for strong denoise ability. '
-              'Only used for the realesr-general-x4v3 model'))
-    parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
-    parser.add_argument(
-        '--model_path', type=str, default=None, help='[Option] Model path. Usually, you do not need to specify it')
-    parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
-    parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
-    parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
-    parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
-    parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
-    parser.add_argument(
-        '--fp32', action='store_true', help='Use fp32 precision during inference. Default: fp16 (half precision).')
-    parser.add_argument(
-        '--alpha_upsampler',
-        type=str,
-        default='realesrgan',
-        help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
-    parser.add_argument(
-        '--ext',
-        type=str,
-        default='auto',
-        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
-    parser.add_argument(
-        '-g', '--gpu-id', type=int, default=None, help='gpu device to use (default=None) can be 0,1,2 for multi-gpu')
+    INPUT_PATH = 'images/'
+    OUTPUT_PATH = 'results'
+    OUTSCALE = 4
+    IMG_SUFFIX = 'out'
+    
+    # parser.add_argument(
+    #     '-n',
+    #     '--model_name',
+    #     type=str,
+    #     default='RealESRGAN_x4plus',
+    #     help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+    #           'realesr-animevideov3 | realesr-general-x4v3'))
 
-    # determine models according to model names
-    # determine models according to model names
+
+
     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
     netscale = 4
     model_path = os.path.join('weights', 'RealESRGAN_x4plus.pth')
+    file_url = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
     if not os.path.isfile(model_path):
-      ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-      model_path = loas_file_from_url(url=file_url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None))
+      # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+      model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
 
     # use dni to control the denoise strength
     dni_weight = None
-    # if args.model_name == 'realesr-general-x4v3' and args.denoise_strength != 1:
-    #     wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
-    #     model_path = [model_path, wdn_model_path]
-    #     dni_weight = [args.denoise_strength, 1 - args.denoise_strength]
 
     # restorer
     upsampler = RealESRGANer(
         scale=netscale,
         model_path=model_path,
         dni_weight=dni_weight,
-        model=model,
-        tile=args.tile,
-        tile_pad=args.tile_pad,
-        pre_pad=args.pre_pad,
-        half=not args.fp32,
-        gpu_id=args.gpu_id)
+        model=model)
 
-    # if args.face_enhance:  # Use GFPGAN for face enhancement
-    #     from gfpgan import GFPGANer
-    #     face_enhancer = GFPGANer(
-    #         model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
-    #         upscale=args.outscale,
-    #         arch='clean',
-    #         channel_multiplier=2,
-    #         bg_upsampler=upsampler)
-    # os.makedirs(args.output, exist_ok=True)
+    model_path = os.path.join('weights', 'GFPGANv1.3.pth')
 
-    if os.path.isfile(args.input):
-        paths = [args.input]
-    else:
-        paths = sorted(glob.glob(os.path.join(args.input, '*')))
+    if not os.path.isfile(model_path):  # Use GFPGAN for face enhancement
+
+        file_url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+        model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
+
+
+
+    face_enhancer = GFPGANer(
+            model_path=model_path,
+            upscale=OUTSCALE,
+            arch='clean',
+            channel_multiplier=2,
+            bg_upsampler=upsampler)
+
+    paths = sorted(glob.glob(os.path.join('images', '*')))
 
     for idx, path in enumerate(paths):
         imgname, extension = os.path.splitext(os.path.basename(path))
         print('Testing', idx, imgname)
 
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
         if len(img.shape) == 3 and img.shape[2] == 4:
             img_mode = 'RGBA'
         else:
             img_mode = None
 
         try:
-            if args.face_enhance:
+            if detect_face(img):
+                print("Is face")
                 _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
             else:
-                output, _ = upsampler.enhance(img, outscale=args.outscale)
+                print("Is not face")
+                output, _ = upsampler.enhance(img, outscale=OUTSCALE)
         except RuntimeError as error:
             print('Error', error)
             print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
         else:
-            if args.ext == 'auto':
-                extension = extension[1:]
-            else:
-                extension = args.ext
+          
+            extension = extension[1:]
+            
             if img_mode == 'RGBA':  # RGBA images should be saved in png format
                 extension = 'png'
-            if args.suffix == '':
-                save_path = os.path.join(args.output, f'{imgname}.{extension}')
-            else:
-                save_path = os.path.join(args.output, f'{imgname}_{args.suffix}.{extension}')
+
+            save_path = os.path.join(OUTPUT_PATH, f'{imgname}_{IMG_SUFFIX}.{extension}')
             cv2.imwrite(save_path, output)
 
 
