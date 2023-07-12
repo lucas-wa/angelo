5b7e0b0fd9a7664af7bdbea1b7024c422f614182
lucas-wa <lucaswanderley600@gmail.com>
2023-07-11 21:12:41 -0300

    Animações de mudança no formulário

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -12,6 +12,41 @@ import numpy as np
 
 from gfpgan import GFPGANer
 
+
+OUTSCALE = 4
+IMG_SUFFIX = 'upscaled'
+model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+netscale = 4
+model_path = os.path.join('weights', 'RealESRGAN_x4plus.pth')
+
+file_url = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
+if not os.path.isfile(model_path):
+  model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
+
+dni_weight = None
+
+# restorer
+upsampler = RealESRGANer(
+    scale=netscale,
+    model_path=model_path,
+    dni_weight=dni_weight,
+    model=model)
+
+
+model_path = os.path.join('weights', 'GFPGANv1.3.pth')
+
+if not os.path.isfile(model_path):  # Use GFPGAN for face enhancement
+    file_url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+    model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
+
+
+face_enhancer = GFPGANer(
+        model_path=model_path,
+        upscale=OUTSCALE,
+        arch='clean',
+        channel_multiplier=2,
+        bg_upsampler=upsampler)
+
 def detect_face(image):
   face_cascade = cv2.CascadeClassifier()
   face_cascade_name = "weights/haarcascade_frontalface_alt.xml"
@@ -35,57 +70,6 @@ def detect_face(image):
 
 
 def upscale_image(image, imgname):
-
-    parser = argparse.ArgumentParser()
-    # INPUT_PATH = image_path
-    OUTPUT_PATH = 'results'
-    OUTSCALE = 4
-    IMG_SUFFIX = 'upscaled'
-    
-    # parser.add_argument(
-    #     '-n',
-    #     '--model_name',
-    #     type=str,
-    #     default='RealESRGAN_x4plus',
-    #     help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
-    #           'realesr-animevideov3 | realesr-general-x4v3'))
-
-
-
-    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-    netscale = 4
-    model_path = os.path.join('weights', 'RealESRGAN_x4plus.pth')
-    file_url = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
-    if not os.path.isfile(model_path):
-      # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-      model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
-
-    # use dni to control the denoise strength
-    dni_weight = None
-
-    # restorer
-    upsampler = RealESRGANer(
-        scale=netscale,
-        model_path=model_path,
-        dni_weight=dni_weight,
-        model=model)
-
-    model_path = os.path.join('weights', 'GFPGANv1.3.pth')
-
-    if not os.path.isfile(model_path):  # Use GFPGAN for face enhancement
-
-        file_url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
-        model_path = load_file_from_url(url=file_url, model_dir=os.path.join('weights'), progress=True, file_name=None)
-
-
-
-    face_enhancer = GFPGANer(
-            model_path=model_path,
-            upscale=OUTSCALE,
-            arch='clean',
-            channel_multiplier=2,
-            bg_upsampler=upsampler)
-
     # paths = sorted(glob.glob(os.path.join('images', '*')))
     npimg = np.fromstring(image, np.uint8)
 
@@ -107,15 +91,13 @@ def upscale_image(image, imgname):
         print('Error', error)
         print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
     else:
-
-        
         extension = imgname.split('.')[1]
         imgname = imgname.split('.')[0]
 
         if img_mode == 'RGBA':  # RGBA images should be saved in png format
             extension = 'png'
 
-        save_path = os.path.join(OUTPUT_PATH, f'{imgname}_{IMG_SUFFIX}.{extension}')
+        save_path = os.path.join('results', f'{imgname}_{IMG_SUFFIX}.{extension}')
         # cv2.imwrite(save_path, output)
 
         return output
--- static/script.js
+++ static/script.js
@@ -1,160 +1,186 @@
 let formState = "generatorLink";
 
 document.querySelector("form")
-    .addEventListener("submit", async e => {
-        e.preventDefault()
+  .addEventListener("submit", async e => {
+    e.preventDefault()
 
-        document.querySelector("button").innerHTML = "<div class='lds-ring'><div></div><div></div><div></div><div></div></div>"
+    console.log(formState)
 
-        if (formState == "generatorLink") {
+    document.querySelector("button").innerHTML = "<div class='lds-ring'><div></div><div></div><div></div><div></div></div>"
 
-            try {
+    if (formState == "generatorLink") {
 
-                const prompt = document.querySelector("#inputPrompt").value
+      try {
 
-                const response = await fetch('/', {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    method: 'POST',
-                    body: JSON.stringify({
-                        'prompt': prompt
-                    })
-                })
+        const prompt = document.querySelector("#inputPrompt").value
 
-                //const data = await response.json();
-                //document.querySelector("img").src = data.image_raw;
-                document.querySelector("button").innerHTML = "Enviar"
-            } catch (error) {
-                console.log(error)
-            }
+        const response = await fetch('/generator', {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            'prompt': prompt,
+            "service": "generator"
+          })
+        })
+
+        if (response.ok) {
+          const data = await response.json();
+          document.querySelector("img").src = data.image_raw;
+        }
+
+        document.querySelector("button").innerHTML = "Enviar"
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+    else if (formState == "upscaleLink") {
+
+      try {
+
+        const input = document.querySelector("#inputFile");
+        const file = input['files'][0]
+        let reader = new FileReader();
+        let base64Image = "";
+
+        reader.readAsDataURL(file);
+
+        reader.onload = async function () {
+          base64Image = reader.result.split(',')[1];
+
+          const response = await fetch('/upscale', {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+              "file": base64Image,
+              "service": "upscale"
+            })
+          })
+
+          if (response.ok) {
+            const data = await response.json();
+
+            const img = document.querySelector("main figure img");
+            img.src = ""
+
+            const figure = document.querySelector("main figure")
+            figure.classList.remove("sr-only");
+            img.src = data.image_raw;
+          }
+          document.querySelector("button").innerHTML = "Enviar"
 
         }
 
-        else if (formState == "upscaleLink") {
 
-            try {
-
-                const input = document.querySelector("#inputFile");
-
-                const formData = new FormData()
-                formData.append('file', input.files[0])
+        console.log(base64Image)
 
 
-                const response = await fetch('/uploadFile', {
-                    method: 'POST',
-                    body: formData
-                })
-                //const data = await response.json();
 
-                //document.querySelector("img").src = data.image_raw;
-                const img = document.querySelector("main figure img");
-                //img.src = ""
 
-                const figure = document.querySelector("main figure")
-                figure.classList.remove("sr-only");
-                //img.src = data.image_raw;
-
-                document.querySelector("button").innerHTML = "Enviar"
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
 
 
 document.querySelectorAll("header nav ul li")
-    .forEach(li => {
-        console.log(li)
-        li.addEventListener("mousedown", e => {
-            const state = e.target.id;
+  .forEach(li => {
+    console.log(li)
+    li.addEventListener("mousedown", e => {
+      const state = e.target.id;
 
-            if (state != formState) handleFormState(state);
-        })
+      if (state != formState) handleFormState(state);
     })
+  })
 
 function handleFormState(state) {
 
 
-    const inputFile = document.querySelector("#inputFile");
-    inputFile.value = '';
-    const p = document.querySelector("#customFileInput p");
-    p.innerHTML = "Faça o upload da imagem";
+  const inputFile = document.querySelector("#inputFile");
+  inputFile.value = '';
+  const p = document.querySelector("#customFileInput p");
+  p.innerHTML = "Faça o upload da imagem";
 
-    const fileInput = document.querySelector("#customFileInput");
-    const inputPrompt = document.querySelector("#inputPrompt");
-    inputPrompt.value = ""
-    const h1 = document.querySelector("main h1");
-    const figure = document.querySelector("main figure");
+  const fileInput = document.querySelector("#customFileInput");
+  const inputPrompt = document.querySelector("#inputPrompt");
+  inputPrompt.value = ""
+  const h1 = document.querySelector("main h1");
+  const figure = document.querySelector("main figure");
 
-    if (state == "generatorLink") {
-        formState = "generatorLink"
-        fileInput.classList.add("slide-out")
+  if (state == "generatorLink") {
+    formState = "generatorLink"
+    fileInput.classList.add("slide-out")
 
-        h1.innerHTML = "Gerador de Imagens"
+    h1.innerHTML = "Gerador de Imagens"
 
-        setTimeout(() => {
-            inputPrompt.classList.add("slide-in");
-            figure.classList.add("slide-in");
-            figure.classList.remove("sr-only")
-            inputPrompt.classList.remove("sr-only");
+    setTimeout(() => {
+      inputPrompt.classList.add("slide-in");
+      figure.classList.add("slide-in");
+      figure.classList.remove("sr-only")
+      inputPrompt.classList.remove("sr-only");
 
-            setTimeout(() => {
-                inputPrompt.classList.remove("slide-in");
-                figure.classList.remove("slide-in");
-            }, 1000)
+      setTimeout(() => {
+        inputPrompt.classList.remove("slide-in");
+        figure.classList.remove("slide-in");
+      }, 1000)
 
-            fileInput.classList.add("sr-only");
-            fileInput.classList.remove("slide-out");
-        }, 1000)
+      fileInput.classList.add("sr-only");
+      fileInput.classList.remove("slide-out");
+    }, 1000)
 
-    }
+  }
 
-    if (state == "upscaleLink") {
-        formState = "upscaleLink"
-        inputPrompt.classList.add("slide-out")
-        figure.classList.add("slide-out")
+  if (state == "upscaleLink") {
+    formState = "upscaleLink"
+    inputPrompt.classList.add("slide-out")
+    figure.classList.add("slide-out")
 
 
-        h1.innerHTML = "Upscale de Imagens"
+    h1.innerHTML = "Upscale de Imagens"
 
-        setTimeout(() => {
-            fileInput.classList.add("slide-in");
-            fileInput.classList.remove("sr-only");
+    setTimeout(() => {
+      fileInput.classList.add("slide-in");
+      fileInput.classList.remove("sr-only");
 
-            setTimeout(() => {
-                fileInput.classList.remove("slide-in");
-            }, 1000)
+      setTimeout(() => {
+        fileInput.classList.remove("slide-in");
+      }, 1000)
 
-            inputPrompt.classList.add("sr-only");
-            figure.classList.add("sr-only");
-            inputPrompt.classList.remove("slide-out");
-            figure.classList.remove("slide-out");
-        }, 1000)
-    }
+      inputPrompt.classList.add("sr-only");
+      figure.classList.add("sr-only");
+      inputPrompt.classList.remove("slide-out");
+      figure.classList.remove("slide-out");
+    }, 1000)
+  }
 
 }
 
 document.querySelector("#inputFile")
-    .addEventListener("change", e => {
+  .addEventListener("change", e => {
 
-        const filepath = e.target.value
-        const arrFilePath = filepath.split(/(\\|\/)/g);
-        let filename = arrFilePath[arrFilePath.length - 1];
+    const filepath = e.target.value
+    const arrFilePath = filepath.split(/(\\|\/)/g);
+    let filename = arrFilePath[arrFilePath.length - 1];
 
-        if (filename.length > 30) {
-            const arrFile = filename.split(".")
-            let name = arrFile[0]
-            let mime = arrFile[1]
+    if (filename.length > 30) {
+      const arrFile = filename.split(".")
+      let name = arrFile[0]
+      let mime = arrFile[1]
 
-            name = name.slice(0, 30) + '[...]';
+      name = name.slice(0, 30) + '[...]';
 
-            filename = name + '.' + mime
-        }
+      filename = name + '.' + mime
+    }
 
 
-        const p = document.querySelector("#customFileInput p");
+    const p = document.querySelector("#customFileInput p");
 
-        p.innerHTML = filename;
+    p.innerHTML = filename;
 
-    })
+  })
