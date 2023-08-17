40ba581058a6d9ef732f0fb110286b6901c7d846
Lucas W. Alves <72520736+lucas-wa@users.noreply.github.com>
2023-08-17 16:54:38 +0000

    Adding animated menu

--- web/src/components/navmenu.jsx
+++ web/src/components/navmenu.jsx
@@ -1,72 +1,73 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { PiPencilSimpleLight, PiRulerFill } from 'react-icons/pi';
+import { MdOutlineImagesearchRoller } from 'react-icons/md';
+import { useEffect, useRef, useState } from 'react';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+
 
 export default function NavMenu() {
+
+  const windowWidth = useRef(window.innerWidth)
+  const menuRef = useRef();
+  const [menuState, setMenuState] = useState(windowWidth.current > 768);
 
 
   const menuItems = [
     {
+      key: 1,
       anchor: "#",
-      item: "Gerador"
+      item: "Gerador",
+      icon: <PiPencilSimpleLight></PiPencilSimpleLight>
     },
     {
+      key: 2,
       anchor: "#",
-      item: "Upascale"
+      item: "Upascale",
+      icon: <PiRulerFill></PiRulerFill>
     },
     {
+      key: 3,
       anchor: "#",
-      item: "Editor"
+      item: "Editor",
+      icon: <MdOutlineImagesearchRoller></MdOutlineImagesearchRoller>
     }
   ]
 
+  function handleMenuState(){
+    setMenuState(prevState => !prevState)
+  }
+
+  useEffect(() => {
+    if(windowWidth.current > 768) {
+      menuRef.current.classList.remove("animate-appear")
+    }
+  },[])
+
   return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-inherit px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300">
-          Menu
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-white" aria-hidden="true" />
-        </Menu.Button>
+    <nav className='relative'>
+
+      <div className='md:sr-only w-10 h-10 flex flex-col justify-center align-center gap-2 cursor-pointer' onClick={handleMenuState}>
+        <span className='w-full h-px block bg-white rounded-md'></span>
+        <span className='w-full h-px block bg-white rounded-md'></span>
+        <span className='w-full h-px block bg-white rounded-md'></span>
       </div>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-content origin-top-right rounded-md bg-inherit  shadow-lg ring-1 ring-white ring-opacity-100 focus:outline-none">
-          <div className="py-1">
-            {
-              menuItems.map(({anchor, item}) =>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href={anchor}
-                      className={classNames(
-                        active ? 'text-white': 'text-gray-300',
-                        'block px-4 py-2 text-sm',
-                        'transition-all',
-                        'px-10'
-                      )}
-                    >
-                      {item}
-                    </a>
-                  )}
-                </Menu.Item>
-              )
-            }
+      {
+        menuState &&
+        <ul ref={menuRef} className="ring-white ring-1 p-5 border rounded-md absolute right-0 animate-appear md:border-transparent md:flex-row">
+          {
+            menuItems.map(({ key, anchor, item, icon }) =>
+              <li className="py-1 w-full">
+                <a href={anchor} key={key} onClick={e => e.preventDefault()}
+                  className="flex justify-start items-center gap-2 brightness-75 hover:brightness-100 transition-all">
+                  {icon}
+                  {item}
+                </a>
+              </li>
+            )
+          }
+        </ul>
+      }
 
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+    </nav>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      animation: {
+        'appear': 'wiggle .5s ease',
+      },
+
+      keyframes: {
+        wiggle: {
+          '0%': {
+            transform: 'translateY(10px)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          },
+
+        }
+      }
+    },
   },
   plugins: [],
 }
