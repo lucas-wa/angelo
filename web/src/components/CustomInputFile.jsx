e661a3a3412a9e948bbc59b17229a5147c672865
lucas-wa <lucaswanderley600@gmail.com>
2023-08-31 20:12:51 -0300

    Responsive image added to upscale

--- web/src/components/CustomInputFile.jsx
+++ web/src/components/CustomInputFile.jsx
@@ -2,7 +2,7 @@ import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 import { useRef, useState } from "react"
 
-export function CustomInputFile() {
+export function CustomInputFile({ handleImage, imgRef }) {
 
     const [fileName, setFileName] = useState("Faça o upload da imagem");
     const inputRef = useRef();
@@ -11,6 +11,21 @@ export function CustomInputFile() {
         const filepath = inputRef.current.value
         const arrFilePath = filepath.split(/(\\|\/)/g);
         let filename = arrFilePath[arrFilePath.length - 1];
+
+        setFileName("Faça o upload da imagem");
+
+        const fileType = inputRef.current['files'][0].type;
+
+        if (fileType !== 'image/png' && fileType !== 'image/jpeg') {
+            alert("Arquivo não suportado. Suba arquivos .png ou .jpg");
+            inputRef.current.value = '';
+            if (inputRef.current.value) {
+                inputRef.current.type = "text";
+                inputRef.current.type = "file";
+            }
+            return null
+        }
+
 
         if (filename.length > 30) {
             const arrFile = filename.split(".")
@@ -22,26 +37,44 @@ export function CustomInputFile() {
             filename = name + '.' + mime
         }
 
-        setFileName(filename)
+        setFileName(filename);
+
+        handleBase64Img();
     }
+
+    function handleBase64Img() {
+        const file = inputRef.current['files'][0]
+        let reader = new FileReader();
+
+        reader.onload = async function (e) {
+            const base64Image = e.target.result;
+
+            handleImage(base64Image);
+        }
+
+        reader.readAsDataURL(file);
+
+    }
+
+
 
     return (
         <>
-            <label 
-            htmlFor="dropzone-file" 
-            className="w-full p-3 bg-white/30 backdrop-blur rounded outline-none placeholder-white/50 text-white focus:pl-4 transition-all ring-1 ring-white cursor-pointer text-7xl flex items-center justify-center flex-col">
+            <label
+                htmlFor="dropzone-file"
+                className="w-full p-3 bg-white/30 backdrop-blur rounded outline-none placeholder-white/50 text-white focus:pl-4 transition-all ring-1 ring-white cursor-pointer text-7xl flex items-center justify-center flex-col">
                 <AiOutlineCloudUpload></AiOutlineCloudUpload>
                 <p className='text-xl'>{fileName}</p>
             </label>
 
-            <input 
-            ref={inputRef} 
-            type="file" 
-            id="dropzone-file" 
-            placeholder="Faça o upload da imagem" 
-            className="hidden" 
-            onChange={e => handleFileName()} 
-            accept="image/png, image/jpeg"/>
+            <input
+                ref={inputRef}
+                type="file"
+                id="dropzone-file"
+                placeholder="Faça o upload da imagem"
+                className="hidden"
+                onChange={e => handleFileName()}
+                accept="image/png, image/jpeg" />
         </>
     )
 }
--- web/src/components/UpscaleMain.jsx
+++ web/src/components/UpscaleMain.jsx
@@ -1,19 +1,76 @@
+import { useRef } from 'react';
 import { CustomInputFile } from './CustomInputFile';
 
 
 export function UpscaleMain() {
+
+    const imgRef = useRef();
+    const imgContainer = useRef();
+
+    function handleImage(base64Img) {
+
+        const virtualImg = new Image();
+
+        virtualImg.onload = function () {
+            const [width, height] = makeImgResponsive(virtualImg);
+
+            imgRef.current.classList.remove("hidden")
+
+            imgRef.current.src = virtualImg.src;
+
+            imgRef.current.width = width;
+            imgRef.current.height = height;
+            
+        }
+
+        virtualImg.src = base64Img
+
+    }
+
+    function makeImgResponsive(virtualImg) {
+
+        let width, height;
+
+        const virtualWidth = virtualImg.width;
+        const virtualHeight = virtualImg.height;
+
+        const aspectRatio = virtualWidth / virtualHeight;
+
+        const widthIsBigger = virtualWidth > virtualHeight;
+
+        console.log(imgContainer.current.offsetWidth, typeof imgContainer.current.style.width)
+
+        if (widthIsBigger) {
+            width = imgContainer.current.offsetWidth - 20
+            height = width / aspectRatio
+        }
+        else {
+            height = imgContainer.current.offsetHeight - 20;
+            width = height * aspectRatio;
+        }
+
+        return [width, height]
+    }
+
     return (
         <div key={2} className="w-full h-auto flex flex-grow gap-10 py-10 px-16 items-center justify-center flex-col-reverse md:flex-row animate-slide-left">
             <form action="" className="w-full flex-1 items-center justify-center flex flex-col gap-5">
 
-                <CustomInputFile></CustomInputFile>
+                <CustomInputFile handleImage={handleImage} imgRef={imgRef}>
+                </CustomInputFile>
 
                 <button className="w-full h-full p-3 text-center rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 brightness-90 hover:brightness-100 transition-all">
                     Enviar
                 </button>
             </form>
 
-            <div className="w-full h-auto bg-white/30 backdrop-blur rounded flex-1 aspect-square ring-1 ring-white">
+            <div ref={imgContainer} className="w-full h-auto flex justify-center items-center bg-white/30 backdrop-blur rounded flex-1 aspect-square ring-1 ring-white">
+                <img
+                    src=""
+                    alt="Imagem do upload"
+                    className="hidden rounded"
+                    ref={imgRef}
+                />
             </div>
         </div>
     )
